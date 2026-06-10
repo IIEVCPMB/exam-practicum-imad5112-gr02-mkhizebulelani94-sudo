ff9ab96daba2337ab0095096478220f5a7df2159
mkhizebulelani94-sudo <mkhizebulelani94@gmail.com>
2026-06-10 09:34:39 +0200

    4th commit

--- app/src/main/java/com/example/campsite_commander/MainActivity.kt
+++ app/src/main/java/com/example/campsite_commander/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.campsite_commander
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -14,9 +15,30 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.campsite_commander.ui.theme.Campsite_CommanderTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val items = arrayOf("Tent", "Marshmellows", "Flashlight", "Knife")
+
+    private val categories = arrayOf("Shelter", "Food", "Safety", "Safety")
+
+    private val quantities = arrayOf("1", "3", "2")
+
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        showWelcomeScreen()
+    }
+
+    //function for the splash screen of the application
+    private fun showWelcomeScreen(){
+        setContentView(R.layout.splash_screen)
+        val startButton = findViewById<Button>(R.id.btnStart)
+        startButton.setOnClickListener {
+
+        }
 
     }
 }
